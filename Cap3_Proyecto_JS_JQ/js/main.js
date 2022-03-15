f9f6199d97885d0f7f09a25ede5c48901d0aba97
julian <julanfzh@gmail.com>
2022-03-15 12:52:57 -0500

    Clase 137 de 292

--- Cap3_Proyecto_JS_JQ/js/main.js
+++ Cap3_Proyecto_JS_JQ/js/main.js
@@ -150,7 +150,16 @@ $(document).ready(function () {
     });
   }
 
+  //Acordeon
   if (window.location.href.indexOf("about") > -1) {
     $("#acordeon").accordion();
+  }
+
+  //Reloj
+  if (window.location.href.indexOf("reloj") > -1) {
+    setInterval(function () {
+      var reloj = moment().format("hh:mm:ss");
+      $("#reloj").html(reloj);
+    }, 1000);
   }
 });
