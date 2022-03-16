ef792604177bdf68e0d97e0dd1b938243534aa7b
julian <julanfzh@gmail.com>
2022-03-16 14:49:18 -0500

    Clase 154 de 292

--- Cap4_TypeScript_Y_POO_JS/js/03-tipos-de-datos.js
+++ Cap4_TypeScript_Y_POO_JS/js/03-tipos-de-datos.js
@@ -24,3 +24,27 @@ var years = [12, 13, 14];
 console.log(years);
 var years_day = [12, "Lunes", 13, "martes", 14]; //array de lo que sea...
 console.log(years_day);
+//MULTIPLES TIPOS DE DATOS
+var multiple = "un valor"; // de esta forma recibe uno o el otro 
+console.log(multiple);
+multiple = 12;
+console.log(multiple); //no salta error por que acepta numeros tambien, lo que no pasaria con un bool
+var con_tipo_mio = "letras o numeros";
+console.log(con_tipo_mio);
+con_tipo_mio = 3232;
+console.log(con_tipo_mio);
+// tipo Let VS Var **********
+/*
+    cuando se declara con let es una variable en bloque
+    cuando se declara con var es una variable global
+
+    EJEMPLO:
+*/
+var numero1 = 12;
+var numero2 = 14;
+if (numero1 == 12) {
+    var numero1_1 = 44; //como se declara con let no cambia su valor al primero declarado como
+    var numero2 = 55; // este que si se declara con var
+    console.log(numero1_1, " / ", numero2); //muestra 44 y 55 
+}
+console.log(numero1, " /  ", numero2); //muestra 12 yy 55
--- Cap4_TypeScript_Y_POO_JS/js/03-tipos-de-datos.ts
+++ Cap4_TypeScript_Y_POO_JS/js/03-tipos-de-datos.ts
@@ -38,3 +38,40 @@ console.log(years);
 
 let years_day: any[] = [12, "Lunes",13, "martes",14]; //array de lo que sea...
 console.log(years_day);
+
+
+//MULTIPLES TIPOS DE DATOS
+let multiple: string | number = "un valor"; // de esta forma recibe uno o el otro 
+console.log(multiple);
+multiple = 12;
+console.log(multiple); //no salta error por que acepta numeros tambien, lo que no pasaria con un bool
+
+
+//CREAR UN TIPO DE DATO
+//Con este se puede crear un tipo de dato que acepte lo que yo nececite
+type letras_o_numeros = string | number;
+
+let con_tipo_mio : letras_o_numeros = "letras o numeros";
+console.log(con_tipo_mio);
+con_tipo_mio = 3232;
+console.log(con_tipo_mio);
+
+// tipo Let VS Var **********
+/* 
+    cuando se declara con let es una variable en bloque 
+    cuando se declara con var es una variable global
+
+    EJEMPLO:
+*/
+
+var numero1 = 12;
+var numero2 = 14;
+
+if(numero1 == 12){
+    let numero1 = 44;  //como se declara con let no cambia su valor al primero declarado como
+    var numero2 = 55; // este que si se declara con var
+
+    console.log(numero1, " / ",numero2); //muestra 44 y 55 
+}
+
+console.log(numero1, " /  " ,numero2) //muestra 12 yy 55
