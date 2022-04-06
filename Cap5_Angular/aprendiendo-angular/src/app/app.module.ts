3abb4be3bd5c6dde5091e70221519cfc497d4da8
julian <julanfzh@gmail.com>
2022-04-06 14:09:13 -0500

    Clase 187 de 292 - Routing

--- Cap5_Angular/aprendiendo-angular/src/app/app.module.ts
+++ Cap5_Angular/aprendiendo-angular/src/app/app.module.ts
@@ -1,24 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms'; //este sirve para hacer el data binding (el inpunt que se muestra y cambia valores a una propiedad reactiva/instantanea)
+import { routing, appRoutingProviders } from './app.routing';
+
 
 import { AppComponent } from './app.component';
 import { VideojuegoComponent } from './videojuego/videojuego.component';
 import { ZapatillasComponent } from './zapatillas/zapatillas.component';
 import { CursosComponent } from './cursos/cursos.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     VideojuegoComponent,
     ZapatillasComponent,
-    CursosComponent
+    CursosComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    routing
   ],
-  providers: [],
+  providers: [
+    appRoutingProviders
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ Cap5_Angular/aprendiendo-angular/src/app/app.routing.ts
@@ -0,0 +1,27 @@
+//Importar modulos del router de angular
+import { ModuleWithProviders } from '@angular/core';
+import { Routes, RouterModule, Route } from '@angular/router';
+
+//Importar componentes
+import { ZapatillasComponent } from './zapatillas/zapatillas.component';
+import { VideojuegoComponent } from './videojuego/videojuego.component';
+import { CursosComponent } from './cursos/cursos.component';
+import { HomeComponent } from './home/home.component';
+
+//Array de rutas
+//constante llamada tipo Routes
+const appRoutes: Routes = [
+    //ruta del navegador , el componente que se va a mostrar
+    //el vacio es para que inicie con este componente y el home es uno que se creo normaalito...
+    {path: ' ', component: HomeComponent},
+    {path: 'zapatillas', component: ZapatillasComponent},
+    {path: 'videojuego', component: VideojuegoComponent},
+    {path: 'cursos', component: CursosComponent},
+    //cuando no encuentra ninguno de los componentes, se ejecuta el componente que yo le diga
+    //!Debe ser la ultima ruta en declararse o sino puede que hayan problemas...
+    {path: '**', component: HomeComponent}
+];
+
+//Exportar el modulo del router 
+export const appRoutingProviders: any[] = [];
+export const routing: ModuleWithProviders<Route> =   RouterModule.forRoot(appRoutes);
